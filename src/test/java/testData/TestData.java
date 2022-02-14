43c9d9f163a2fc4e75c94c5c7ba9a26110b6d6ea
daria <daria.sheianova@mail.ru>
2022-02-14 17:42:16 +0100

    Initial commit

---
+++ src/test/java/testData/TestData.java
@@ -0,0 +1,132 @@
+package testData;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class TestData {
+
+    public static RequestSpecification requestSpecBaseUrl = new RequestSpecBuilder().
+            setBaseUri("https://restful-booker.herokuapp.com/booking/").
+            build();
+
+    public static String authenticationRequestBody = "{\n" +
+            "    \"username\" : \"admin\",\n" +
+            "    \"password\" : \"password123\"\n" +
+            "}";
+
+    //Block with requests
+    public static String createNewBookingRequest_JohnMacintosh = "{" +
+            "\"firstname\" : \"John\",\n" +
+            "    \"lastname\" : \"Macintosh\",\n" +
+            "    \"totalprice\" : 100,\n" +
+            "    \"depositpaid\" : true,\n" +
+            "    \"bookingdates\" : {\n" +
+            "        \"checkin\" : \"2022-06-01\",\n" +
+            "        \"checkout\" : \"2022-06-21\"\n" +
+            "    },\n" +
+            "    \"additionalneeds\" : \"\"}";
+
+
+    public static String createNewBookingRequest_DeeJay = "{" +
+            "\"firstname\" : \"Dee\",\n" +
+            "    \"lastname\" : \"Jay\",\n" +
+            "    \"totalprice\" : 90,\n" +
+            "    \"depositpaid\" : true,\n" +
+            "    \"bookingdates\" : {\n" +
+            "        \"checkin\" : \"2022-08-01\",\n" +
+            "        \"checkout\" : \"2022-08-07\"\n" +
+            "    },\n" +
+            "    \"additionalneeds\" : \"\"}";
+
+    public static String createNewBookingRequest_BloomyReachson = "{" +
+            "\"firstname\" : \"Bloomy\",\n" +
+            "    \"lastname\" : \"Reachson\",\n" +
+            "    \"totalprice\" : 40,\n" +
+            "    \"depositpaid\" : true,\n" +
+            "    \"bookingdates\" : {\n" +
+            "        \"checkin\" : \"2022-05-20\",\n" +
+            "        \"checkout\" : \"2022-05-22\"\n" +
+            "    },\n" +
+            "    \"additionalneeds\" : \"Cat\"}";
+
+    public static String updateExistingBooking_Reachson = "{" +
+            "\"firstname\" : \"Bloomy\",\n" +
+            "    \"lastname\" : \"Reachson\",\n" +
+            "    \"totalprice\" : 1040,\n" +
+            "    \"depositpaid\" : true,\n" +
+            "    \"bookingdates\" : {\n" +
+            "        \"checkin\" : \"2022-05-20\",\n" +
+            "        \"checkout\" : \"2022-05-29\"\n" +
+            "    },\n" +
+            "    \"additionalneeds\" : \"Cat\"}";
+
+
+    public static String updateLastNameRequest_DeeJay = "{\n" +
+            "    \"lastname\" : \"Jayson\"\n" +
+            "}";
+
+
+    private String token = "";
+
+    private int bookingId_Macintosh;
+    private int bookingId_DeeJay;
+    private int bookingId_Reachson;
+
+
+    //Methods
+    public void authenticate(){
+        String token =
+
+        given().
+            contentType(ContentType.JSON).
+            body(authenticationRequestBody).
+            log().all().
+        when().
+            post("https://restful-booker.herokuapp.com/auth").
+        then().
+            assertThat().
+            statusCode(200).
+            extract().
+            path("token");
+
+        setToken(token);
+    }
+
+    //Block with getters and setters
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public int getBookingId_Macintosh() {
+        return bookingId_Macintosh;
+    }
+
+    public void setBookingId_Macintosh(int bookingId_Macintosh) {
+        this.bookingId_Macintosh = bookingId_Macintosh;
+    }
+
+    public int getBookingId_DeeJay() {
+        return bookingId_DeeJay;
+    }
+
+    public void setBookingId_DeeJay(int bookingId_DeeJay) {
+        this.bookingId_DeeJay = bookingId_DeeJay;
+    }
+
+    public int getBookingId_Reachson() {
+        return bookingId_Reachson;
+    }
+
+    public void setBookingId_Reachson(int bookingId_Reachson) {
+        this.bookingId_Reachson = bookingId_Reachson;
+    }
+}
---
+++ src/test/java/tests/Tests.java
@@ -0,0 +1,134 @@
+package tests;
+
+import io.restassured.http.ContentType;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import testData.TestData;
+
+import static io.restassured.RestAssured.*;
+import static org.testng.Assert.assertTrue;
+import static testData.TestData.*;
+
+public class Tests {
+
+    TestData testData = new TestData();
+
+    @Test
+    public void checkCreateBooking_JohnMacintosh(){
+        int bookingId_Macintosh =
+
+        given().
+            contentType(ContentType.JSON).
+            spec(requestSpecBaseUrl).
+            body(createNewBookingRequest_JohnMacintosh).
+            log().all().
+        when().
+            post().
+        then().
+            assertThat().
+            statusCode(200).
+            extract().
+            path("bookingid");
+
+        testData.setBookingId_Macintosh(bookingId_Macintosh);
+        System.out.println(testData.getBookingId_Macintosh());
+    }
+
+    @Test
+    public void checkCreateBooking_DeeJay(){
+        int bookingId_DeeJay =
+
+        given().
+            contentType(ContentType.JSON).
+            spec(requestSpecBaseUrl).
+            body(createNewBookingRequest_DeeJay).
+            log().all().
+        when().
+            post().
+        then().
+            assertThat().
+            statusCode(200).
+            extract().
+            path("bookingid");
+
+        testData.setBookingId_DeeJay(bookingId_DeeJay);
+    }
+
+    @Test
+    public void checkCreateBooking_BloomyReachson(){
+        int bookingId_Reachson =
+
+        given().
+            contentType(ContentType.JSON).
+            spec(requestSpecBaseUrl).
+            body(createNewBookingRequest_BloomyReachson).
+            log().all().
+        when().
+            post().
+        then().
+            assertThat().
+            statusCode(200).
+            extract().
+            path("bookingid");
+
+        testData.setBookingId_Reachson(bookingId_Reachson);
+    }
+
+    @Test
+    public void checkDeleteBooking_Macintosh(){
+        testData.authenticate();
+
+        given().
+            contentType(ContentType.JSON).
+            spec(requestSpecBaseUrl).
+            cookie("token", testData.getToken()).
+            log().all().
+        when().
+            delete(String.valueOf(testData.getBookingId_Macintosh())).
+        then().
+            assertThat().
+            statusCode(201);
+    }
+
+    @Test
+    public void checkUpdateSurname_DeeJay(){
+        String newLastName =
+
+        given().
+            contentType(ContentType.JSON).
+            cookie("token", testData.getToken()).
+            spec(requestSpecBaseUrl).
+            body(updateLastNameRequest_DeeJay).
+            log().all().
+        when().
+            patch(String.valueOf(testData.getBookingId_DeeJay())).
+        then().
+            assertThat().
+            statusCode(200).
+            extract().
+            path("lastname");
+
+        assertTrue(newLastName.equals("Jayson"));
+    }
+
+    @Test
+    public void checkUpdateBooking_Reachson(){
+        testData.authenticate();
+
+        given().
+            contentType(ContentType.JSON).
+            cookie("token", testData.getToken()).
+            spec(requestSpecBaseUrl).
+            body(updateExistingBooking_Reachson).
+            log().all().
+        when().
+            put(String.valueOf(testData.getBookingId_Reachson())).
+        then().
+            assertThat().
+            statusCode(200).
+            extract().
+            path("lastname");
+
+    }
+}
